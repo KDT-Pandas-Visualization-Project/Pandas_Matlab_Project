c33439a2ebbc6126e37d253603c32960f6542cc6
Myeong no ah <42794561+noah2397@users.noreply.github.com>
2024-01-19 20:35:15 +0900

    add sum comment on init file

--- SampleData.py
+++ SampleData.py
@@ -2,5 +2,8 @@ import pandas as pd
 import numpy as np
 
 # 주피터 파일에서 읽어온 데이터를 import할 수 없어, 파이썬 파일에서 읽어온 데이터 사용
-data = pd.read_csv(r'..\Data\anime-dataset-2023.csv')
-print(data.head(2))
+anime = pd.read_csv(r"..\Data\anime-dataset-2023.csv")
+user_details = pd.read_csv(r"..\Data\users-details-2023.csv")
+user_score=pd.read_csv(r"..\Data\users-score-2023.csv")
+anime_filter=pd.read_csv(r"..\Data\anime-filtered.csv")
+print("Complete!")
