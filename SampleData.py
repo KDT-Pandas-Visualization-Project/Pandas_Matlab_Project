07fe9cb425e50eeee970a85c1d01b8b17d8d0c02
Myeong no ah <42794561+noah2397@users.noreply.github.com>
2024-01-22 21:11:02 +0900

    resolve sampleData

--- SampleData.py
+++ SampleData.py
@@ -115,6 +115,7 @@ anime["Duration"]=anime["Duration"].apply(delete_per_ep)
 #=========================================================================
 #==========================user_details변수 전처리========================
 #=========================================================================
+
 user_details = user_details.drop(["Joined", "Days Watched", "Watching", "On Hold", "Dropped", "Plan to Watch", "Rewatched", "Episodes Watched"], axis = 1)
 user_details[["Mean Score", "Completed", "Total Entries"]] = user_details[["Mean Score", "Completed", "Total Entries"]].fillna(0)
 user_details["Username"] = user_details["Username"].fillna("user")
@@ -127,6 +128,7 @@ user_details.rename({"Mal ID":"user_id"}, inplace = True, axis=True)
 user_details["Total Entries"] = user_details["Total Entries"].astype("int64")
 user_details["Mean Score"] = user_details["Mean Score"].astype("float32")
 user_details["Gender"] = user_details["Gender"].astype("category")
+
 #=========================================================================
 #===========================user_score변수 전처리=========================
 #=========================================================================
